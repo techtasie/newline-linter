e2f9753af88393baa05c789315de4cd3cf6e9d4f
Tim Wendt <techtasie@gmail.com>
2023-06-04 15:41:31 +0200

    now working

--- index.js
+++ index.js
@@ -4,28 +4,26 @@ const fs = require('fs-extra');
 const { program } = require('commander');
 
 program
-  .version('1.0.0')
+  .version('1.0.3')
   .description('Ensure there is an empty line at the end of a file')
   .arguments('<file>')
-  .action(file => {
-    fs.readFile(file, 'utf8')
-      .then(content => {
-        const trimmedContent = content.trim();
-        const lastCharacter = trimmedContent.charAt(trimmedContent.length - 1);
-
-        if (lastCharacter !== '\n') {
-          console.error(`Error: Missing empty line at the end of ${file}`);
-          process.exit(1);
-        } else {
-          console.log(`Empty line found at the end of ${file}`);
-        }
-      })
-      .catch(err => {
-        console.error(`Error: Failed to read file ${file}`);
-        console.error(err);
+  .action((file) => {
+    try {
+      const content = fs.readFileSync(file, 'utf8');
+      const lines = content.split('\n');
+      const lastLine = lines[lines.length - 1];
+      
+      if (lastLine.length === 0) {
+        console.log(`Empty line found at the end of ${file}`);
+      } else {
+        console.error(`Error: Missing empty line at the end of ${file}`);
         process.exit(1);
-      });
+      }
+    } catch (err) {
+      console.error(`Error: Failed to read file ${file}`);
+      console.error(err);
+      process.exit(1);
+    }
   });
 
 program.parse(process.argv);
-
