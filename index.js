a5eb4ce5e67272366f409cb1cb8e7687f64d9efc
Tim Wendt <techtasie@gmail.com>
2023-06-04 15:46:22 +0200

    added multifile support

--- index.js
+++ index.js
@@ -4,24 +4,32 @@ const fs = require('fs-extra');
 const { program } = require('commander');
 
 program
-  .version('1.0.3')
+  .version('1.0.4')
   .description('Ensure there is an empty line at the end of a file')
-  .arguments('<file>')
-  .action((file) => {
-    try {
-      const content = fs.readFileSync(file, 'utf8');
-      const lines = content.split('\n');
-      const lastLine = lines[lines.length - 1];
-      
-      if (lastLine.length === 0) {
-        console.log(`Empty line found at the end of ${file}`);
-      } else {
-        console.error(`Error: Missing empty line at the end of ${file}`);
-        process.exit(1);
+  .arguments('<files...>')
+  .action((files) => {
+    let hasError = false;
+    
+    files.forEach((file) => {
+      try {
+        const content = fs.readFileSync(file, 'utf8');
+        const lines = content.split('\n');
+        const lastLine = lines[lines.length - 1];
+        
+        if (lastLine.length === 0) {
+          console.log(`Empty line found at the end of ${file}`);
+        } else {
+          console.error(`Error: Missing empty line at the end of ${file}`);
+          hasError = true;
+        }
+      } catch (err) {
+        console.error(`Error: Failed to read file ${file}`);
+        console.error(err);
+        hasError = true;
       }
-    } catch (err) {
-      console.error(`Error: Failed to read file ${file}`);
-      console.error(err);
+    });
+    
+    if (hasError) {
       process.exit(1);
     }
   });
